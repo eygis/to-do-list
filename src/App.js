f40cadc5a791f3eba7b7007b0fd27ff81bd55a34
eygis <86146074+eygis@users.noreply.github.com>
2021-10-01 16:23:09 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    }
+  }
+  
+  addTask = (e) => {
+    e.preventDefault()
+    let taskValue = document.getElementById("task").value;
+    //console.log(taskValue)
+    this.setState({
+      tasks: [...this.state.tasks, taskValue]
+    })
+    document.getElementById("inputArea").reset();
+    console.log(this.state)
+  } 
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div id="wrapper">
+   <div id="banner">
+      <h1>To Do List</h1>
+    </div>
+    <div id="listArea">
+    <form id="inputArea" onSubmit={this.addTask}>
+    <input id="task" type="text" placeholder="input task here!"/>
+    <button type="submit">Add</button>
+    </form>
+    <div id="list">
+      <ul>
+    {this.state.tasks.map((e) => <li><Pad text={e} /></li>) }
+    </ul>
+    </div>
+    </div>
     </div>
   );
+  }
+}
+
+class Pad extends React.Component {
+  
+  render () {
+    const { text } = this.props;
+    return (
+      <div class="toDoBlock">
+        {text}
+      </div>
+    )
+  }
 }
 
 export default App;
