dd97463d5a361bc2347a69690f89950b9412dd55
eygis <86146074+eygis@users.noreply.github.com>
2021-10-03 11:02:59 +0900

    added option to add tasks to a "completed" list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: []
+      tasks: [],
+      completed: []
     }
     //this.deleteFunction = this.deleteFunction.bind(this)
   }
@@ -28,20 +29,37 @@ class App extends React.Component {
       tasks: this.state.tasks.filter(task => task !== e)
     })
     }
+
+  completeFunction = (e) => {
+    this.setState({
+      tasks: this.state.tasks.filter(task => task !== e),
+      completed: [...this.state.completed, e] 
+    })
+  }
   render() {
   return (
    <div id="wrapper">
    <div id="banner">
       <h1>To Do List</h1>
     </div>
-    <div id="listArea">
     <form id="inputArea" onSubmit={this.addTask}>
     <input id="task" type="text" placeholder="input task here!" required/>
     <button type="submit">Add</button>
     </form>
+    <div id="listArea">
+    
     <div id="list">
+      <h2>To Do:</h2>
       <ul>
-    {this.state.tasks.map((e) => <li key={e} id={e}><div className="bar"><Pad text={e} /><div className="deleteX" onClick={() => {this.deleteFunction(e)}}>X</div></div></li>) }
+    {this.state.tasks.map((e) => <li key={e} id={e}><div className="bar"><Pad text={e} />
+    <div className="yes" onClick={() => {this.completeFunction(e)}}>&#10003;</div>
+    <div className="deleteX" onClick={() => {this.deleteFunction(e)}}>X</div></div></li>) }
+    </ul>
+    </div>
+    <div id="complete">
+    <h2>Completed:</h2>
+    <ul>
+    {this.state.completed.map((e) => <li key={e} id={e}><div className="bar"><Pad text={e} /></div></li>)}
     </ul>
     </div>
     </div>
